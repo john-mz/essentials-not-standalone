923297f0f86c01fb7aa1240a0b2e723b3beda429
Stiven <johnstiven40@gmail.com>
2025-08-25 09:03:11 -0500

    implementation of module with modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser'; //WITHOUT THIS app wont start
-import { FormsModule } from '@angular/forms';
 
 import { App } from './app';
 import { Header } from './header/header';
 import { User } from './user/user';
-import { Tasks } from './tasks/tasks';
-import { Task } from './tasks/task/task';
-import { NewTask } from './tasks/new-task/new-task';
 import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
-  declarations: [App, Header, User, Tasks, Task, NewTask], //modules
+  declarations: [App, Header, User], //modules
   // con bootstrap indicamos cual es nuestro root component
   bootstrap: [App], //root
 
   //remember to import BrowserModule              
   imports: [
-    CommonModule, BrowserModule, FormsModule, SharedModule
+    CommonModule, BrowserModule, SharedModule, TasksModule
   ]
   // BrowserModule ya incluye DatePipe
 })
